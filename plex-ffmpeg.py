061f9ee97ba9f2e5cdef088f72e49c0907159720
RABC <RABCbot@users.noreply.github.com>
2020-03-12 15:33:52 -0400

    Added Flask

--- plex-ffmpeg.py
+++ plex-ffmpeg.py
@@ -3,15 +3,20 @@ import requests
 import xml.etree.ElementTree as ET
 import subprocess
 import logging
+from flask import Flask
+import json
 
-logging.basicConfig(filename='plex_stereo.log',level=logging.INFO)
+api = Flask(__name__)
+logging.basicConfig(filename='plex_stereo.log',level=logging.DEBUG)
 
-plex_token = "<your-token>"
-plex_url = "http://<your-ip>:32400/library/sections/1/all"
+plex_token = "<your plex token>"
+plex_url = "http://<your pelx server>:32400/library/sections/1/all"
 headers    = {"X-Plex-Token": plex_token,
               "Content-Type": "application/xml"}
+ok = [{"ok"}]
 
-def Transcode():
+@api.route('/transcode', methods=['GET'])
+def transcode():
   response = requests.get(plex_url, headers=headers)
   response.raise_for_status()
   root = ET.fromstring(response.text)
@@ -23,21 +28,28 @@ def Transcode():
         file_split = os.path.splitext(part.attrib["file"])
         in_file = file_split[0] + file_split[1]
         out_file = file_split[0] + "_stereo" + file_split[1]
-        logging.debug("Transcoding:", video.attrib["title"], in_file)
+        logging.info("Transcoding:", video.attrib["title"], in_file)
         if not os.path.exists(out_file):
           subprocess.call(["ffmpeg", "-i", in_file, "-ac", "2", "-af", "pan=stereo|FL=FC+0.30*FL+0.30*BL|FR=FC+0.30*FR+0.30*BR", "-c:v", "copy", out_file])
+  return json.dumps(ok)
 
 
-def CheckSubtitle():
+@api.route('/subtitle', methods=['GET'])
+def get_subtitle():
   response = requests.get(plex_url, headers=headers)
   response.raise_for_status()
   root = ET.fromstring(response.text)
+  titles, files = [], []
   for video in root.findall("./Video"):
     part = video.find("./Media/Part")
     file_split = os.path.splitext(part.attrib["file"])
     if not os.path.exists(file_split[0] + ".srt"):
-      print(file_split)
+      # print(video.attrib["title"], "\t", part.attrib["file"])
+      titles.append(video.attrib["title"])
+      files.append(part.attrib["file"])
+  titles_files = [{"title": t, "file": f} for t, f in zip(titles, files)]
+  return json.dumps(titles_files)
 
-
-CheckSubtitle()
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=8080)
 
